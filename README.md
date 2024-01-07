22ac9d6e42eb0d2ce9189405114a0dca1cce03b0
Wambaforestin <wambaforestin@gmail.com>
2024-01-07 12:22:06 +0100

    mini project custom form validation

---
+++ README.md
@@ -0,0 +1,37 @@
+# Custom Form Validation
+
+This project is part of my `60-day learning journey` for beginners.
+
+## Description
+
+The Custom Form Validation project is a JavaScript application that provides form validation functionality for web forms. It ensures that user input meets specific criteria before submitting the form.
+
+## Features
+
+- Validates user input for required fields, email addresses, passwords, etc.
+- Provides real-time feedback to users on invalid input.
+- Prevents form submission until all input is valid.
+- Customizable error messages and validation rules.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/Wambaforestin/custom-form-validation.git`
+2. Open the `index.html` file in your web browser.
+
+## Usage
+
+1. Fill out the form fields.
+2. Observe the real-time validation feedback.
+3. Submit the form once all input is valid.
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions, bug reports, or feature requests, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+- [Inspiring Course](https://www.w3schools.com/js) - Provided guidance on form validation techniques.
---
+++ script.js
@@ -0,0 +1,81 @@
+// creating the varaibles for the different error messages
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+var passwordError = document.getElementById("password-error");
+
+// creating the function that will validate the name[name, surname]
+function validateName() {
+    var name = document.getElementById("name").value;
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Please enter your name and surname';
+        return false;
+    }
+
+    nameError.innerHTML = "<i class='bx bxs-check-circle' ></i>";
+    return true;
+}
+
+// creating the function that will validate the phone
+function validatePhone() {
+    var phone = document.getElementById("phone").value;
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone is required';
+        return false;
+    }
+    if (phone.length != 9) {
+        phoneError.innerHTML = 'Phone number should have 9 digits';
+        return false;
+    }
+    if (!phone.match(/^[0-9]{9}$/)) {
+        phoneError.innerHTML = 'Only digits please';
+        return false;
+    }
+
+    phoneError.innerHTML = "<i class='bx bxs-check-circle' ></i>";
+    return true;
+}
+
+// creating the function that will validate the email
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Invalid Email';;
+        return false;
+    }
+    emailError.innerHTML = "<i class='bx bxs-check-circle' ></i>";
+    return true;
+}
+
+function validateMessage() {
+    var message = document.getElementById("message").value;
+    var messageRequired = 30;
+    var messageLeft = messageRequired - message.length;
+
+    if (messageLeft > 0) {
+        messageError.innerHTML = messageLeft + ' More characters required to complete the message';
+        return false;
+    }
+    messageError.innerHTML = "<i class='bx bxs-check-circle' ></i>";
+    return true;
+}
+//function to validate the form
+
+function validateForm() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+        // submitError.style.display = 'block';
+        submitError.innerHTML = 'Please enter the required fields before submitting the form';
+        setTimeout(function () { submitError.style.display = 'none'; }, 3000);
+        return false;
+    }
+}
